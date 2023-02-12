a96e27ad71c5a68502c2b7e60b1bfbe3b0b0d691
WestGhostTrip <98178922+WestGhostTrip@users.noreply.github.com>
2023-02-12 20:12:50 +0300

    Update Homework4.js

---
+++ Homework4.js
@@ -0,0 +1,11 @@
+function compareArrays(arr1, arr2) {
+  let result;
+  result = (arr1.length === arr2.length) && arr1.every((element, index) => element === arr2[index]);
+  return result; 
+}
+
+function advancedFilter(arr) {
+  let resultArr;
+  resultArr = arr.filter((number) => number > 0).filter((number) => number % 3 === 0).map((number) => number * 10);
+  return resultArr; 
+}
